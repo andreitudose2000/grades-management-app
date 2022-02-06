f4f726730e1d6f530e1011c8d6ee03a37b472f35
Andrei Tudose <andreitudose2000@gmail.com>
2022-02-06 23:40:42 +0200

    23:40

--- client/src/pages/PublicConfigPage.tsx
+++ client/src/pages/PublicConfigPage.tsx
@@ -2,23 +2,8 @@ import * as React from "react";
 import { Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { State, Year } from "../redux/interfaces";
-import CoursesTable from "../components/base/CoursesTable";
+import CoursesTable from "../components/business/CoursesTable";
 
-export default function PublicConfigPage() {
-  const years: Year[] = useSelector((state: State) => state.years);
-  return (
-    <>
-      {years.map((year, yIdx) => (
-        <>
-          <Typography variant="h4">{`Anul ${yIdx}`}</Typography>
-          {year.semesters.map((semester, sIdx) => (
-            <>
-              <Typography variant="h6">{`Semestrul ${sIdx}`}</Typography>
-              <CoursesTable data={semester.courses} />
-            </>
-          ))}
-        </>
-      ))}
-    </>
-  );
+export default function ConfigsPage() {
+  return <></>;
 }
